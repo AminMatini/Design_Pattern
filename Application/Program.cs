7754b936a16a655a0f9fcfa37adf4f1997e0f147
Amin Matini <Aminmatini78@gmail.com>
2022-09-25 15:27:21 +0330

    feat : use of builder pattern

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Application.BuilderPatternSample.ConcreteBuilder;
+
+Console.WriteLine("Builder Sample");
+var simpleRoom = new SimpleRoomBuilder().Build();
+simpleRoom.Describe();
+
+var familyRoom = new FamilyRoomBuilder().Build();
+familyRoom.Describe();
+
+
+Console.ReadKey();
