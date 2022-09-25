cb8debb41974f624dd8adac7e68641d25794b70f
Amin Matini <Aminmatini78@gmail.com>
2022-09-25 14:59:44 +0330

    Add project files.

---
+++ Application/BuilderPatternSample/ConcreteBuilder/SimpleRoomBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Application.BuilderPatternSample.BuilderInterfaces;
+using Application.BuilderPatternSample.Product;
+
+namespace Application.BuilderPatternSample.ConcreteBuilder;
+
+public class SimpleRoomBuilder : IRoomBuilder
+{
+    public Room Build()
+    {
+        return new Room("Simple Room")
+            .WithBed(1);
+    }
+}
